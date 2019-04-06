0f8552a74a62e3df29796d44b0c2ee57f78e3cb8
shahninad <ninads78@gmail.com>
2019-04-06 10:26:08 +0530

    Init commit

---
+++ build/precache-manifest.3a1098976ec42a3e7c276e00f78e37e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14a50d300d2e27afbc27",
+    "url": "/static/css/main.a9cccbba.chunk.css"
+  },
+  {
+    "revision": "14a50d300d2e27afbc27",
+    "url": "/static/js/main.44162066.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2510800228ab53ab2ce38a27a20a0a9f",
+    "url": "/index.html"
+  }
+];
